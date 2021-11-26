07ca7f289a88d21b1046adf96e500046bfc36ef6
ayoubbelatrous <77838146+ayoubbelatrous@users.noreply.github.com>
2021-11-26 18:39:12 +0100

    pyramid rendering

--- Isotope/src/Panels/ScenePyramid.cpp
+++ Isotope/src/Panels/ScenePyramid.cpp
@@ -1,17 +1,67 @@
 #include "ScenePyramid.h"
 #include "imgui/imgui.h"
-void ScenePyramid::Init()
-{
 
+#include "Scene/Components.h"
+
+
+
+void ScenePyramid::DrawEntity(Entity entity) {
+
+
+	ImGui::PushStyleColor(ImGuiCol_Header, ImVec4(0.25f, 0.25f, 0.25f, 1.00f));
+	auto& tag = entity.GetComponent<TagComponent>().Name;
+	ImGuiTreeNodeFlags flags = ((selection == entity) ? ImGuiTreeNodeFlags_Selected : 0) | ImGuiTreeNodeFlags_OpenOnArrow;
+	flags |= ImGuiTreeNodeFlags_SpanAvailWidth;
+	flags |= ImGuiTreeNodeFlags_FramePadding;
+	ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(3.f, 3.f));
+	ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0.f, 0.f));
+	bool opened = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, tag.c_str());
+	ImGui::PopStyleVar();
+	ImGui::PopStyleVar();
+	if (ImGui::IsItemClicked())
+	{
+		selection = entity;
+	}
+
+	bool entityDeleted = false;
+	if (ImGui::BeginPopupContextItem())
+	{
+		if (ImGui::MenuItem("Delete Entity"))
+			entityDeleted = true;
+
+		ImGui::EndPopup();
+	}
+
+	if (opened)
+	{
+		ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_SpanAvailWidth;
+		bool opened = ImGui::TreeNodeEx((void*)9817239, flags, tag.c_str());
+		if (opened)
+			ImGui::TreePop();
+		ImGui::TreePop();
+	}
+
+	ImGui::PopStyleColor();
+
+}
+
+
+
+void ScenePyramid::Init(Lithium::Ref<Scene> scene)
+{
+	_context = scene;
+	selection = {};
 }
 
 void ScenePyramid::RenderUI()
 {
 	ImGui::Begin("Pyramid");
-
-
-
-
+	_context->GetReg().each([&](auto entityID)
+	{
+		Entity entity{ entityID , _context.get() };
+		DrawEntity(entity);
+		
+	});
 
 
 	ImGui::End();
--- Isotope/src/Panels/ScenePyramid.h
+++ Isotope/src/Panels/ScenePyramid.h
@@ -1,11 +1,16 @@
 #pragma once
+#include "Scene/Scene.h"
+#include "Scene/Entity.h"
 
 class ScenePyramid
 {
 public:
-	void Init();
+	void Init(Lithium::Ref<Scene> scene);
 	void RenderUI();
+	void DrawEntity(Entity entity);
+	Entity selection;
 private:
-
+	Lithium::Ref<Scene> _context;
+	
 };
 
